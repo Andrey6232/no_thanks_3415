9e084e336e01c58859c75a695f99d399148777e2
Nndd2 <cool.andrey2005@gmail.com>
2024-10-28 12:05:17 +0300

    Учтены фишки игрока

--- src/player.py
+++ src/player.py
@@ -5,13 +5,14 @@ from src.hand import Hand
 
 
 class Player:
-    def __init__(self, name: str, hand: Hand, score: int = 0):
+    def __init__(self, name: str, hand: Hand, score: int = 0, chips: int = 0):
         self.name = name
         self.hand = hand
         self.score = score
+        self.chips = chips
 
     def __str__(self):
-        return f'{self.name}({self.score}): {self.hand}'
+        return f'{self.name}({self.chips}): {self.hand}'
 
     def __eq__(self, other: typing.Self | str | dict):
         if isinstance(other, str):
@@ -26,9 +27,10 @@ class Player:
         return {
             'name': self.name,
             'hand': str(self.hand),
-            'score': self.score
+            'score': self.score,
+            'chips': self.chips
         }
 
     @classmethod
     def load(cls, data: dict):
-        return cls(name=data['name'], hand=Hand.load(data['hand']), score=int(data['score']))
+        return cls(name=data['name'], hand=Hand.load(data['hand']), score=int(data['score']), chips=int(data['chips']))
--- test/test_player.py
+++ test/test_player.py
@@ -4,31 +4,32 @@ from src.player import Player
 
 def test_init():
     h = Hand.load('10 3 20')
-    p = Player(name='Alex', hand=h, score=15)
+    p = Player(name='Alex', hand=h, score=h.score(), chips=11)
     assert p.name == 'Alex'
     assert p.hand == h
-    assert p.score == 15
+    assert p.score == 33
+    assert p.chips == 11
 
 def test_str():
     h = Hand.load('10 3 20')
-    p = Player(name='Alex', hand=h, score=15)
-    assert str(p) == 'Alex(15): 10 3 20'
+    p = Player(name='Alex', hand=h, score=h.score(), chips=11)
+    assert str(p) == 'Alex(11): 10 3 20'
 
 def test_save():
     h = Hand.load('10 3 20')
-    p = Player(name='Alex', hand=h, score=15)
-    assert p.save() == {'name': 'Alex', 'score': 15, 'hand': '10 3 20'}
+    p = Player(name='Alex', hand=h, score=h.score(), chips=11)
+    assert p.save() == {'name': 'Alex', 'score': 33, 'chips': 11, 'hand': '10 3 20'}
 
 def test_eq():
     h1 = Hand.load('10 3 20')
     h2 = Hand.load('10 3 20')
-    p1 = Player(name='Alex', hand=h1, score=15)
-    p2 = Player(name='Alex', hand=h2, score=15)
+    p1 = Player(name='Alex', hand=h1, score=h1.score(), chips=11)
+    p2 = Player(name='Alex', hand=h2, score=h2.score(), chips=11)
     assert p1 == p2
 
 def test_load():
-    data = {'name': 'Alex', 'score': 15, 'hand': '10 3 20'}
+    data = {'name': 'Alex', 'score': 33, 'chips': 11, 'hand': '10 3 20'}
     h = Hand.load('10 3 20')
-    p_expected = Player(name='Alex', hand=h, score=15)
+    p_expected = Player(name='Alex', hand=h, score=h.score(), chips=11)
     p = Player.load(data)
     assert p == p_expected
