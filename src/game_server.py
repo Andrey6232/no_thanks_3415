4cd7d7379e021cc34467793d6e3a57f8ff435f26
Nndd2 <cool.andrey2005@gmail.com>
2024-11-25 01:06:55 +0300

    Дописан game server

--- src/game_server.py
+++ src/game_server.py
@@ -1,6 +1,6 @@
 import inspect
 import json
-import sys
+
 
 from src.deck import Deck
 from src.game_state import GameState
@@ -9,13 +9,13 @@ from src.player import Player
 from src.player_interaction import PlayerInteraction
 import src.player_interactions as all_player_types
 
-import logging
+
 
 import enum
 
 
 class GamePhase(enum.StrEnum):
-    DRAW_EXTRA = "Draw extra card"
+    START_BIDDING = "Draw extra card"
     BIDDING = "Choose to pay or take card"
     DECLARE_WINNER = "Declare a winner"
     GAME_END = "Game ended"
@@ -84,35 +84,86 @@ class GameServer:
         current_phase = GamePhase.BIDDING
         while current_phase != GamePhase.GAME_END:
             phases = {
-                GamePhase.DRAW_EXTRA: self.draw_extra,
+                GamePhase.START_BIDDING: self.start_bidding_phase,
                 GamePhase.BIDDING: self.bidding_phase,
                 GamePhase.DECLARE_WINNER: self.declare_winner_phase
             }
             current_phase = phases[current_phase]()
 
     def declare_winner_phase(self) -> GamePhase:
-        print(f"{self.game_state.current_player()} is the winner!")
+        score = self.game_state.score_players()
+        winner = min(score, key=lambda k: score[k])
+        print(f"{winner, score[winner]} is the winner!")
         return GamePhase.GAME_END
 
+    def start_bidding_phase(self):
+        if self.game_state.deck == Deck([]):
+            return GamePhase.DECLARE_WINNER
+        self.game_state.card = self.game_state.deck.draw_card()
+        self.game_state.score = 0
+        print('Top:', {"card": self.game_state.card, "score": self.game_state.score})
+        return GamePhase.BIDDING
+
     def bidding_phase(self):
-        pass
+        current_player = self.game_state.current_player()
+        interaction = self.player_types[current_player.name]
+        choose = interaction.choose_action(current_player, self.game_state.chips)
+        if choose == 'Take card':
+            self.game_state.take_card()
+            interaction.inform_card_take(current_player)
+            return GamePhase.START_BIDDING
+        elif choose == 'Spend':
+            self.game_state.to_pay()
+            interaction.inform_player_spend(current_player)
+            self.game_state.next_player()
+            return GamePhase.BIDDING
 
-    def draw_extra(self):
-        pass
-
-    def inform_all(self):
-        pass
+    def inform_all(self, method: str, *args, **kwargs):
+        for p in self.player_types.values():
+            getattr(p, method)(*args, **kwargs)
 
     @staticmethod
     def request_player_count() -> int:
-        pass
+        while True:
+            try:
+                player_count = int(input('How many players?\n'))
+                if 3 <= player_count <= 5:
+                    return player_count
+            except ValueError:
+                pass
+            print('Please input a number between 3 and 5')
 
     @staticmethod
     def request_player() -> (str, PlayerInteraction):
-        pass
+        player_types = []
+        for name, cls in inspect.getmembers(all_player_types):
+            if inspect.isclass(cls) and issubclass(cls, PlayerInteraction):
+                player_types.append(cls.__name__)
+        player_types_as_str = ', '.join(player_types)
+
+        while True:
+            name = input('How to call a player?\n')
+            if name.isalpha():
+                break
+            print('Name must be a single word, alphabetic characters only')
+
+        while True:
+            try:
+                kind = input(f'What kind of player is it ({player_types_as_str})?\n')
+                kind = getattr(all_player_types, kind)
+                break
+            except AttributeError:
+                print(f'Allowed player types are: {player_types_as_str}\n')
+        return name, kind
 
 def __main__():
-    pass
+    load_from_file = False
+    if load_from_file:
+        server = GameServer.load_game()
+    else:
+        server = GameServer.new_game(GameServer.get_players())
+    server.save()
+    server.run()
 
 if __name__ == "__main__":
     __main__()
