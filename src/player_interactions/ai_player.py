2234d20ddb7f8066de18b4aba2830ec256b4dd18
Nndd2 <cool.andrey2005@gmail.com>
2024-11-25 01:20:39 +0300

    Добавлен бот

--- src/player_interactions/ai_player.py
+++ src/player_interactions/ai_player.py
@@ -1,5 +1,26 @@
+from src.player import Player
 from src.player_interaction import PlayerInteraction
+import random
 
 
 class Bot(PlayerInteraction):
-    pass
+    @classmethod
+    def choose_action(cls, player: Player):
+        if player.chips > 0:
+            return random.choice(['Take card', 'Spend'])
+        else:
+            return 'Take card'
+
+    @classmethod
+    def inform_card_take(cls, player: Player):
+        """
+        Сообщает, что игрок взял карту
+        """
+        pass
+
+    @classmethod
+    def inform_card_to_pay(cls, player: Player):
+        """
+        Сообщает, что игрок использовал фишку
+        """
+        pass
