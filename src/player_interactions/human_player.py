5d64080e3adff18a9baf7582902b1d2059fe1d19
Nndd2 <cool.andrey2005@gmail.com>
2024-12-06 23:44:41 +0300

    Добавлена функция выбора действия

--- src/player_interactions/human_player.py
+++ src/player_interactions/human_player.py
@@ -1,5 +1,19 @@
+from src.player import Player
 from src.player_interaction import PlayerInteraction
 
 
 class Human(PlayerInteraction):
-    pass
+    @classmethod
+    def choose_action(cls, player: Player):
+        while True:
+            print("Choose action: type 't' (take card) or 's' (spend chips)")
+            action = input()
+            if action == 't':
+                return 'Take card'
+            elif action == 's':
+                if player.chips <= 0:
+                    print("You don't have enough chips, choose another action")
+                    continue
+                return 'Spend'
+            else:
+                print('Incorrect input')
